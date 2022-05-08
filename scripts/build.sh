7afd8175beed70bf1a0a338d92b04fee33d38e61
Cavoi Rom <cavoirom.wanted@yahoo.com>
2022-05-08 20:39:42 +0700

    Fixed wrong execute path of gradlew script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -17,11 +17,11 @@ JRE8_FILE="$PROJECT_DIRECTORY/.gradle/java/jre8.tar.gz"
 JRE8_FILE_SHA512="c62002aa1a28547b0b0cefea3f83af2cf6507002bf296b1b83bd04406f4c362f4c74940209ebbd3803513395bec7503882aac7f8d35dcbf6a6e02e9155a53cdd  $JRE8_FILE"
 
 if ! echo "$JRE8_FILE_SHA512" | shasum -c; then
-  mkdir -p "$PROJECT_DIRECTORY/.gradle/java"
-  echo "Downloading JRE8 for ProGuard: $JRE8_URL"
-  curl -s -L "$JRE8_URL" -o "$JRE8_FILE"
-  tar -C "$PROJECT_DIRECTORY/.gradle/java/" -xvzf "$JRE8_FILE"
+  mkdir -p "$PROJECT_DIRECTORY/.gradle/java" \
+      && echo "Downloading JRE8 for ProGuard: $JRE8_URL" \
+      && curl -s -L "$JRE8_URL" -o "$JRE8_FILE" \
+      && tar -C "$PROJECT_DIRECTORY/.gradle/java/" -xvzf "$JRE8_FILE"
 fi
 
-"$PROJECT_DIRECTORY"/gradlew buildPlugin proGuard
+gradle buildPlugin proGuard
 
