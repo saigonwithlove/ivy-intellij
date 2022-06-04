7b6bb3e22f18dfdd6f0062331e78655b12d89747
Cavoi Rom <cavoirom.wanted@yahoo.com>
2022-06-04 13:23:43 +0700

    Spit download JRE 8 into another script.

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,20 +8,10 @@ set -o pipefail
 SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 PROJECT_DIRECTORY="$SCRIPT_DIRECTORY/.."
 
-# Go to project directory
+# Setup Proguard binary, our build depends on it.
+bash "$SCRIPT_DIRECTORY/setup-jre8.sh"
+
+# Go to project directory and build
 cd "$PROJECT_DIRECTORY"
-
-# Download JRE to use rt.jar
-JRE8_URL="https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u332-b09/OpenJDK8U-jre_x64_linux_hotspot_8u332b09.tar.gz"
-JRE8_FILE="$PROJECT_DIRECTORY/.gradle/java/jre8.tar.gz"
-JRE8_FILE_SHA512="c62002aa1a28547b0b0cefea3f83af2cf6507002bf296b1b83bd04406f4c362f4c74940209ebbd3803513395bec7503882aac7f8d35dcbf6a6e02e9155a53cdd  $JRE8_FILE"
-
-if ! echo "$JRE8_FILE_SHA512" | shasum -c; then
-  mkdir -p "$PROJECT_DIRECTORY/.gradle/java" \
-      && echo "Downloading JRE8 for ProGuard: $JRE8_URL" \
-      && curl -s -L "$JRE8_URL" -o "$JRE8_FILE" \
-      && tar -C "$PROJECT_DIRECTORY/.gradle/java/" -xvzf "$JRE8_FILE"
-fi
-
 gradle buildPlugin proGuard
 
