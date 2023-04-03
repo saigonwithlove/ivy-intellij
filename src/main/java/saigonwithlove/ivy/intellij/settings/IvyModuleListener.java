3550c0604c5e933244965a7641337eb186a8c1ad
Huy Phan <pbhuy93@gmail.com>
2023-04-04 01:57:37 +0700

    31: bugfix AlreadyDisposedException

---
+++ src/main/java/saigonwithlove/ivy/intellij/settings/IvyModuleListener.java
@@ -0,0 +1,47 @@
+package saigonwithlove.ivy.intellij.settings;
+
+import static saigonwithlove.ivy.intellij.settings.PreferenceService.State;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.ModuleListener;
+import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.NotNull;
+import saigonwithlove.ivy.intellij.shared.IvyModule;
+import saigonwithlove.ivy.intellij.shared.Modules;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class IvyModuleListener implements ModuleListener {
+  @Override
+  public void moduleRemoved(@NotNull Project project, @NotNull Module module) {
+    PreferenceService preferenceService = project.getService(PreferenceService.class);
+    List<IvyModule> initialModulesCopy =
+        new ArrayList<>(preferenceService.getState().getIvyModules());
+    List<IvyModule> remainingModules =
+        initialModulesCopy.stream()
+            .filter(item -> !item.getName().equalsIgnoreCase(module.getName()))
+            .collect(Collectors.toList());
+    preferenceService.update(
+        (state -> {
+          state.setIvyModules(remainingModules);
+          return state;
+        }));
+  }
+
+  @Override
+  public void moduleAdded(@NotNull Project project, @NotNull Module module) {
+    Optional<IvyModule> importModule = Modules.toIvyModule(module);
+    if (importModule.isPresent()) {
+      PreferenceService preferenceService = project.getService(PreferenceService.class);
+      List<IvyModule> allModules = new ArrayList<>(preferenceService.getState().getIvyModules());
+      allModules.add(importModule.get());
+      preferenceService.update(
+          (state -> {
+            state.setIvyModules(allModules);
+            return state;
+          }));
+    }
+  }
+}
